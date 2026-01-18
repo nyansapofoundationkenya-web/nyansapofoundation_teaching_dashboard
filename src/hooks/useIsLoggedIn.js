8344f4ae0a09bc50ff36f1c12169601c38688f5b
Victor Mutunga <mutunga.victor21@students.dkut.ac.ke>
2026-01-18 22:23:48 +0300

    pipeline test

--- src/hooks/useIsLoggedIn.js
+++ src/hooks/useIsLoggedIn.js
@@ -4,9 +4,5 @@ import { useSelector } from "react-redux";
 export function useIsLoggedIn() {
   // const { user, loading } = useSelector((state) => state.auth);
   
-  // return {
-  //   user,
-  //   loading,
-  //   isLoggedIn: !!user?.uid
-  // };
+  
 }
